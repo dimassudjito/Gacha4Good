87f4d2febb9305dc20a965952a7d38d665adb90d
Michael Kwok <michael.kwok00@gmail.com>
2022-01-15 16:26:36 -0700

    Move GQL types

--- backend/src/gql.js
+++ backend/src/gql.js
@@ -1,1 +1,29 @@
 import { gql } from "apollo-server";
+
+const types = gql`
+    type User {
+        userId: ID!
+        username: String!
+        balance: Number!
+        # inventory: Inventory
+    }
+
+    interface GameData {
+        gameId: ID!
+        name: String!
+    }
+
+    type BoxingKingGameData implements GameData {
+        gameId: ID!
+        name: String!
+        cards: [BoxingKingGameCard!]!
+        shop: [BoxingKingCardPack!]
+    }
+
+    type GameRoom {
+        gameRoomId: ID!
+        startTime: Date!
+        endTime: Date
+        players: [User]
+    }
+`;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,24 @@
-const { ApolloServer, PubSub } = require('apollo-server')
-const mongoose = require('mongoose')
+import { ApolloServer } from "apollo-server";
+import mongoose from "mongoose";
 
-require('dotenv').config()
-const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
-
-const PORT = process.env.PORT || 5555
+const PORT = process.env.PORT || 5555;
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req })
-})
+    types,
+    resolvers,
+});
 
 mongoose
-  .connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log('MongoDB: database connected')
-    return server.listen({ port: PORT })
-  })
-  .then((res) => {
-    console.log(`Server is running at ${res.url}`)
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+    .connect(
+        "mongodb+srv://gacha4good:verygood@cluster0.bpxxb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    )
+    .then(() => {
+        console.log("MongoDB: database connected");
+        return server.listen({ port: PORT });
+    })
+    .then((res) => {
+        console.log(`Server is running at ${res.url}`);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
