e8cc39a788cfdafc9c2d797f51361cd666d268db
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-15 14:03:09 -0700

    GAC-27 setup redux, plus example of reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+// Redux
+import { Provider } from 'react-redux'
+import { store } from './state/store'
+
 // Default font: ubuntu
 import '@fontsource/ubuntu/300.css'
 import '@fontsource/ubuntu/400.css'
@@ -10,7 +14,9 @@ import '@fontsource/ubuntu/700.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
