3f0d2e913bff950b78313d1e15dca42212eb9203
acl-24 <85856412+acl-24@users.noreply.github.com>
2022-01-15 15:43:53 -0700

    Add files via upload

---
+++ backend/src/models/index.js
@@ -0,0 +1,30 @@
+const { ApolloServer, PubSub } = require('apollo-server')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
+
+const PORT = process.env.PORT || 5555
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req })
+})
+
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB: database connected')
+    return server.listen({ port: PORT })
+  })
+  .then((res) => {
+    console.log(`Server is running at ${res.url}`)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
