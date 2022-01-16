42564f8367baa3a9bb79463c74eae8bb28224d9f
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-16 01:06:07 -0700

    center card

--- frontend/src/components/boxerKing/ChooseBoxer.js
+++ frontend/src/components/boxerKing/ChooseBoxer.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
-import { Box } from "@mui/material";
+import { Box, IconButton, Grid } from "@mui/material";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 import BoxerCard from "./BoxerCard";
 
@@ -8,10 +10,22 @@ const ChooseBoxer = ({ boxers }) => {
     const [index, setIndex] = useState(0);
 
     return (
-        <Box>
-            {boxers.map((boxer) => (
-                <BoxerCard key={boxer.name} boxer={boxer} />
-            ))}
+        <Box sx={{ mt: 8 }}>
+            <Grid container justifyContent="center" alignItems="center">
+                <Grid item>
+                    <IconButton color="secondary" onClick={() => setIndex(index - 1)}>
+                        <ArrowBackIosNewIcon />
+                    </IconButton>
+                </Grid>
+                <Grid item>
+                    <BoxerCard key={boxers[index].name} boxer={boxers[index]} />
+                </Grid>
+                <Grid item>
+                    <IconButton color="secondary" onClick={() => setIndex(index - 1)}>
+                        <ArrowForwardIosIcon />
+                    </IconButton>
+                </Grid>
+            </Grid>
         </Box>
     );
 };
