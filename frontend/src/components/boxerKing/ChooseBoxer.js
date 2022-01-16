1481b8132f6adf471a0d646daa7e25a3c9341d3f
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-16 00:49:31 -0700

    add more data

--- frontend/src/components/boxerKing/ChooseBoxer.js
+++ frontend/src/components/boxerKing/ChooseBoxer.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Box } from '@mui/material'
 
 import BoxerCard from './BoxerCard'
 
 const ChooseBoxer = ({ boxers }) => {
+  const [index, setIndex] = useState(0)
+
   return (
     <Box>
       {boxers.map((boxer) => (
--- frontend/src/pages/BoxerKing.js
+++ frontend/src/pages/BoxerKing.js
@@ -20,6 +20,22 @@ const dummyBoxerData = [
     power: 20,
     img: 'https://www.biography.com/.image/t_share/MTg0ODYyNTg1Njg0ODI5MzA0/gettyimages-1163293092.jpg',
     head_img: 'https://www.famousbirthdays.com/headshots/manny-pacquiao-3.jpg'
+  },
+  {
+    name: 'Muhammad Ali',
+    hp: 110,
+    power: 20,
+    img: 'https://cdn.vox-cdn.com/thumbor/gSlcQpXEJLH_vkBSovzAYrxhJwA=/77x0:1322x934/1400x1400/filters:focal(77x0:1322x934):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/55911717/1_J85jI_HahBSm4U8FSvCd3A.0.jpeg',
+    head_img:
+      'https://upload.wikimedia.org/wikipedia/commons/8/89/Muhammad_Ali_NYWTS.jpg'
+  },
+  {
+    name: 'Logan Paul',
+    hp: 60,
+    power: 10,
+    img: 'https://akns-images.eonline.com/eol_images/Entire_Site/202154/rs_634x1024-210604143907-634.logan-paul-boxing.ct.jpg?fit=around%7C634:1024&output-quality=90&crop=634:1024;center,top',
+    head_img:
+      'https://yt3.ggpht.com/ytc/AKedOLRzOwb2y7du3TOWgYqGU9rocPuvfgxkdDKH5pzrcg=s900-c-k-c0x00ffffff-no-rj'
   }
 ]
 
@@ -29,7 +45,7 @@ const BoxerKing = () => {
   return (
     <Box>
       <Container maxWidth="md">
-        {true ? <GamePlay boxers={boxers} /> : <ChooseBoxer boxers={boxers} />}
+        {false ? <GamePlay boxers={boxers} /> : <ChooseBoxer boxers={boxers} />}
       </Container>
     </Box>
   )
