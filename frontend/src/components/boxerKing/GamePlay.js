a758a0faebd4f2eb532183af9191f9592f012de1
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-15 19:16:43 -0700

    GAC-4 layout for game

--- frontend/src/components/boxerKing/GamePlay.js
+++ frontend/src/components/boxerKing/GamePlay.js
@@ -1,89 +1,50 @@
 import React, { useState } from 'react'
 
-import { Box, Typography, Card, Button } from '@mui/material'
+import { Box, Typography, Card, Button, Grid, CardContent } from '@mui/material'
 
-const GamePlay = () => {
-  const [P1Move, setP1Move] = useState(null)
-  const [P2Move, setP2Move] = useState(null)
-  const [P1IsChoosing, setP1IsChoosing] = useState(true)
-  const [P2IsChoosing, setP2IsChoosing] = useState(true)
+import BoxerCard from './BoxerCard'
 
-  const makeP1Move = (move) => {
-    setP1Move(move)
-    setP1IsChoosing(false)
-  }
-
-  const makeP2Move = (move) => {
-    setP2Move(move)
-    setP2IsChoosing(false)
-  }
+const GamePlay = ({ boxers }) => {
+  const [p1, setP1] = useState(boxers[0])
+  const [p2, setP2] = useState(boxers[1])
 
   return (
     <Box>
-      {/* <Card sx={{ mb: 2 }}>
-        <Typography>Debugging station</Typography>
-        <Typography>Player 1: {P1Move}</Typography>
-        <Typography>Player 2: {P2Move}</Typography>
-      </Card> */}
-      <Card sx={{ mb: 2 }}>
-        {P1Move.length === null || P1IsChoosing || P2IsChoosing ? (
-          <Typography>Make your move!</Typography>
-        ) : (
-          <Box>
-            P1 ({P1Move}) Vs. P2 ({P2Move})
-          </Box>
-        )}
-      </Card>
-      <Card sx={{ mb: 2 }}>
-        <Typography>Player 1</Typography>
-        <Button
-          onClick={() => {
-            makeP1Move('rock')
-          }}
-        >
-          Rock
-        </Button>
-        <Button
-          onClick={() => {
-            makeP1Move('paper')
-          }}
-        >
-          Paper
-        </Button>
-        <Button
-          onClick={() => {
-            makeP1Move('scissor')
-          }}
-        >
-          Scissor
-        </Button>
-        <Button onClick={() => setP1IsChoosing(true)}>Continue</Button>
-      </Card>
-      <Card>
-        <Typography>Player 2</Typography>
-        <Button
-          onClick={() => {
-            makeP2Move('rock')
-          }}
-        >
-          Rock
-        </Button>
-        <Button
-          onClick={() => {
-            makeP2Move('paper')
-          }}
-        >
-          Paper
-        </Button>
-        <Button
-          onClick={() => {
-            makeP2Move('scissor')
-          }}
-        >
-          Scissor
-        </Button>
-        <Button onClick={() => setP2IsChoosing(true)}>Continue</Button>
-      </Card>
+      <Grid container columnSpacing={1}>
+        <Grid item xs={6}>
+          <Card>
+            <Typography>userId1</Typography>
+            <Typography>HP: {p1.hp}</Typography>
+            <hr />
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            <Typography>userId2</Typography>
+            <Typography>HP: {p2.hp}</Typography>
+            <hr />
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container sx={{ mt: 2 }}>
+        <Grid item xs={4}>
+          <BoxerCard boxer={p1} />
+        </Grid>
+        <Grid item xs={4}>
+          <Button color="secondary" variant="outlined">
+            Rock
+          </Button>
+          <Button color="secondary" variant="outlined">
+            Paper
+          </Button>
+          <Button color="secondary" variant="outlined">
+            Scissor
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <BoxerCard boxer={p2} />
+        </Grid>
+      </Grid>
     </Box>
   )
 }
