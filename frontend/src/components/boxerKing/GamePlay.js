b56af173bd3e291518fb732acbd1ca16624441ed
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-15 21:06:29 -0700

    GAC-4 layout basic flow for attacking opponents hp

--- frontend/src/components/boxerKing/GamePlay.js
+++ frontend/src/components/boxerKing/GamePlay.js
@@ -6,24 +6,50 @@ import BoxerCard from './BoxerCard'
 import BoxerHead from './BoxerHead'
 
 const GamePlay = ({ boxers }) => {
-  const [p1, setP1] = useState(boxers[0])
-  const [p2, setP2] = useState(boxers[1])
+  // general card data
+  const [p1] = useState(boxers[0])
+  const [p2] = useState(boxers[1])
+  // gameplay data
+  const [p1Game, setP1Game] = useState({ hp: p1.hp, move: null })
+  const [p2Game, setP2Game] = useState({ hp: p2.hp, move: null })
+
+  const makeMove = (move) => {
+    // update p1Game
+    setP1Game({ ...p1Game, move })
+    // make move for bot
+    setP2Game({ ...p2Game, move: 'scissor' })
+    // evaluate game
+    let winner = evaluateMove(p1Game.move, p2Game.move)
+    // adjust health point
+    modifyHp(winner)
+  }
+
+  const evaluateMove = (move1, move2) => {
+    return 'p1'
+  }
+
+  const modifyHp = (winner) => {
+    if (winner === 'p1') {
+      const newHp = p2Game.hp - p1.power
+      setP2Game({ ...p2Game, hp: newHp })
+    }
+  }
 
   return (
     <Box>
       <Grid container columnSpacing={1}>
         <Grid item xs={6}>
           <Card>
-            <Typography>userId1</Typography>
-            <Typography>HP: {p1.hp}</Typography>
+            <Typography>Player 1 (Human)</Typography>
+            <Typography>HP: {p1Game.hp}</Typography>
             <hr />
             <BoxerHead boxer={p1} />
           </Card>
         </Grid>
         <Grid item xs={6}>
           <Card>
-            <Typography>userId2</Typography>
-            <Typography>HP: {p2.hp}</Typography>
+            <Typography>Player 2 (Bot)</Typography>
+            <Typography>HP: {p2Game.hp}</Typography>
             <hr />
             <BoxerHead boxer={p2} />
           </Card>
@@ -34,7 +60,13 @@ const GamePlay = ({ boxers }) => {
           <BoxerCard boxer={p1} />
         </Grid>
         <Grid item xs={4}>
-          <Button color="secondary" variant="outlined">
+          <Button
+            color="secondary"
+            variant="outlined"
+            onClick={() => {
+              makeMove('rock')
+            }}
+          >
             Rock
           </Button>
           <Button color="secondary" variant="outlined">
