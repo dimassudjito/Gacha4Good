3a59db09f6fafc7e82ea4bd6e35c42bd0932aaf9
acl-24 <85856412+acl-24@users.noreply.github.com>
2022-01-15 18:38:52 -0700

    Update gql.js

--- backend/src/gql.js
+++ backend/src/gql.js
@@ -1,4 +1,26 @@
 import { gql } from "apollo-server";
+import { gql } from "apollo-server";
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { UserInputError, addErrorLoggingToSchema } = require('apollo-server')
+
+const User = require('../../models/User')
+const {
+  validateRegisterInput,
+  validateLoginInput
+} = require('../../util/validators')
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      name: user.name
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  )
+}
 
 export const types = gql`
     type User {
@@ -71,8 +93,47 @@ export const resolvers = {
         gameRooms: () => {},
     },
     Mutation: {
-        login: async (_, { username, password }) => {},
-        register: async (_, { username, password }) => {},
+        login: async (_, { username, password }) => {
+        // validate user data
+        const { errors, valid } = validateLoginInput(username, password)
+        if (!valid) {
+            throw new UserInputError('Errors', { errors })
+        }
+
+        const user = await User.LogIn(username, password);
+
+        // check if password is correct
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) {
+            throw new UserInputError('Wrong credentials')
+        }
+
+        // issue token
+        const token = generateToken(user)
+
+        return { ...user._doc, id: user._id, token }
+        },
+        
+        register: async (_, { username, password }) => {
+        // validate user data
+        const { errors, valid } = validateRegisterInput(username, password)
+        if (!valid) {
+         throw new UserInputError('Errors', { errors })
+        }
+
+        // check if username is already taken
+        const user = await User.findOne({ username })
+        if (user) {
+            throw new UserInputError('Username is already used')
+        }
+
+        // hash password and get auth token
+        password = await bcrypt.hash(password, 12)
+        const newUser = await User.CreateUser(username,password)
+        const token = generateToken(res)
+
+        return { ...res._doc, id: res._id, token }
+        },
         // UserAddBalance,
         // UserBuyItem,
         // UserDeleteItem,
