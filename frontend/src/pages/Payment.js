34efbc3cacc516e322a0eea5bdc2c75dc20a8802
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-16 10:48:18 -0700

    add payment page

--- frontend/src/pages/Payment.js
+++ frontend/src/pages/Payment.js
@@ -1,7 +1,30 @@
 import React from "react";
+import ReactDOM from "react-dom";
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
-const Payment = () => {
-    return <h1>This is payment page</h1>;
-};
+class Payment extends React.Component {
+    createOrder(data, actions) {
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: "0.01",
+                    },
+                },
+            ],
+        });
+    }
+    onApprove(data, actions) {
+        return actions.order.capture();
+    }
+    render() {
+        return (
+            <PayPalButton
+                createOrder={(data, actions) => this.createOrder(data, actions)}
+                onApprove={(data, actions) => this.onApprove(data, actions)}
+            />
+        );
+    }
+}
 
 export default Payment;
